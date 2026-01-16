0823099422af70693a40cde65de8871c5ddc1427
Ayman Ougri <ao@macbookpro.lan>
2026-01-15 22:17:32 -0500

    huge new shed, big fast api work 7 (cors)

--- Sentinel/main.py
+++ Sentinel/main.py
@@ -4,6 +4,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.gatekeeper.gatekeeper import gatekeeper_router
 load_dotenv()
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://crownpointconsult.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(gatekeeper_router)
